07a46990bc954524c3386b0bafb872e4931e04d8
Long Tran <long.tran@gigacover.com>
2020-12-28 18:17:50 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SimpleOtpInput = () => {
+  return (
+    <View>
+      <Text>SimpleOtpInput</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SimpleOtpInput;
